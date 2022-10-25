fd8829b8fcfbf8fe87d9ea579e84c56d559b4dd9
sheepy0125 <sheepy404@gmail.com>
2022-10-25 17:24:18 -0400

    Get user input and echo it

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@
 /***** Setup *****/
 #![no_std]
 #![no_main]
+
 // Imports
+#[macro_use]
+extern crate fixedvec;
+use fixedvec::FixedVec;
+
+use embedded_hal::serial::Read;
+use nb::block;
 use panic_halt as _;
-// Constants
-const INCREASE_DELAY_BY: u16 = 10;
+use ufmt::{uWrite, uwriteln};
+
+const MAXIMUM_LENGTH: usize = 32;
 
 /***** Main *****/
 #[arduino_hal::entry]
@@ -19,21 +27,42 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
     let mut led = pins.d13.into_output();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    // Blink with an ever-increasing delay
-    let mut delay_ms = 0;
+    // Setup
+    let mut new_line_received = false;
+    let mut preallocated_space = alloc_stack!([char; MAXIMUM_LENGTH]);
+    let mut buffer = FixedVec::new(&mut preallocated_space);
+
+    uwriteln!(&mut serial, "Ready for calculations!").unwrap();
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(match led.is_set_high() {
-            true => 50,
-            false => {
-                delay_ms += INCREASE_DELAY_BY; // pro tip: do this less than 6554 times
-                delay_ms
+        // Fetch
+        uWrite::write_str(&mut serial, "Enter an equation > ").unwrap();
+        led.set_low();
+        while !new_line_received {
+            match block!(serial.read()).unwrap() {
+                10 => new_line_received = true,
+                byte => buffer
+                    .push(byte.try_into().unwrap_or('?'))
+                    .unwrap_or_else(|_| {
+                        // The only error that this can return is one of no space left
+                        new_line_received = true;
+                    }),
             }
-        });
-
-        if delay_ms >= 1000 {
-            panic!("Delay was too much!");
         }
+        led.set_high();
+
+        // TODO: processing
+
+        // Output
+        uWrite::write_str(&mut serial, "Received: ").unwrap();
+        buffer
+            .iter()
+            .for_each(|char| uWrite::write_char(&mut serial, *char).unwrap());
+        uWrite::write_char(&mut serial, '\n').unwrap();
+
+        // Reset
+        buffer.clear();
+        new_line_received = false;
     }
 }
